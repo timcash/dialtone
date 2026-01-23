6054696986d4993f74a1dfa83b5a653e3a47a0a1
Tim <timcash@gmail.com>
2026-01-23 15:16:39 -0800

    chore: start ticket ticket-start-final-check

---
+++ tickets/ticket-start-final-check/test/integration_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "testing"
+
+func TestIntegration_Example(t *testing.T) {
+	t.Log("Integration test for ticket_start_final_check")
+	t.Fatal("Not implemented")
+}
---
+++ tickets/ticket-start-final-check/test/unit_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "testing"
+
+func TestUnit_Example(t *testing.T) {
+	t.Log("Unit test for ticket_start_final_check")
+	t.Fatal("Not implemented")
+}
