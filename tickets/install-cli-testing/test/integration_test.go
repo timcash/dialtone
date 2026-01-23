3ae5450ad4684c7096c12bfdcd82047f1011cf69
Tim <timcash@gmail.com>
2026-01-23 15:22:03 -0800

    chore: start ticket install-cli-testing

--- tickets/install-cli-testing/test/integration_test.go
+++ tickets/install-cli-testing/test/integration_test.go
@@ -3,6 +3,6 @@ package test
 import "testing"
 
 func TestIntegration_Example(t *testing.T) {
-	t.Log("Integration test for camera_plugin_demo")
+	t.Log("Integration test for install_cli_testing")
 	t.Fatal("Not implemented")
 }
