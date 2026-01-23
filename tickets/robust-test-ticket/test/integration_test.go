c429b08ba2ede1731f8cfb6afbd3df54fe3dd28d
Tim <timcash@gmail.com>
2026-01-23 15:17:59 -0800

    chore: start ticket robust-test-ticket

---
+++ tickets/robust-test-ticket/test/integration_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "testing"
+
+func TestIntegration_Example(t *testing.T) {
+	t.Log("Integration test for robust_test_ticket")
+	t.Fatal("Not implemented")
+}
---
+++ tickets/robust-test-ticket/test/unit_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "testing"
+
+func TestUnit_Example(t *testing.T) {
+	t.Log("Unit test for robust_test_ticket")
+	t.Fatal("Not implemented")
+}
