2d41d44db074a5a223972c9af71fbc3b238a914c
Tim <timcash@gmail.com>
2026-01-23 18:11:27 -0800

    chore: start ticket remote-camera-test

---
+++ tickets/remote-camera-test/test/integration_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "fmt"
+
+func RunIntegration() error {
+	fmt.Println("Running integration test for remote_camera_test")
+	return nil
+}
---
+++ tickets/remote-camera-test/test/unit_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "fmt"
+
+func RunUnit() error {
+	fmt.Println("Running unit test for remote_camera_test")
+	return nil
+}
