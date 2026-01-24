1b31a198a9570d19a55021816b72abf91fce4ff0
Tim <timcash@gmail.com>
2026-01-23 17:30:48 -0800

    chore: start ticket integrate-ffmpeg-camera-fix

---
+++ tickets/integrate-ffmpeg-camera-fix/test/e2e_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "fmt"
+
+func RunE2E() error {
+	fmt.Println("Running E2E test for integrate_ffmpeg_camera_fix")
+	return nil
+}
---
+++ tickets/integrate-ffmpeg-camera-fix/test/integration_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "fmt"
+
+func RunIntegration() error {
+	fmt.Println("Running integration test for integrate_ffmpeg_camera_fix")
+	return nil
+}
