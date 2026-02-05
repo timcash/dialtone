4bf5e65a0b1a69056443487d6ea200f5b71d98d9
Tim <timcash@gmail.com>
2026-02-05 13:23:03 -0800

    cleanup

---
+++ src/plugins/swarm/test/types.d.ts
@@ -0,0 +1,8 @@
+declare module 'autobase';
+declare module 'hyperbee';
+declare module 'hypercore';
+declare module 'b4a';
+declare module 'bare-fs';
+declare module 'bare-path';
+declare module 'bare-os';
+declare module 'ram';
