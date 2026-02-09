df26ba530eeb30a923fbef958e8118e825d46838
Tim <timcash@gmail.com>
2026-02-09 14:58:41 -0800

    nix updates

--- src/plugins/nix/cli/cli.go
+++ src/plugins/nix/cli/cli.go
@@ -1,21 +1,32 @@
 package cli
 
 import (
-	"fmt"
 	"dialtone/cli/src/plugins/nix/test"
+	"flag"
+	"fmt"
 )
 
 func Run(args []string) error {
-	if len(args) < 2 {
-		return fmt.Errorf("usage: nix smoke <dir>")
+	if len(args) == 0 {
+		return fmt.Errorf("usage: nix <command> [args]\n\nCommands:\n  smoke <dir> [--smoke-timeout <sec>]  Run automated UI tests\n  lint                                 Lint Go and TypeScript code")
 	}
 
 	command := args[0]
-	dir := args[1]
-
 	switch command {
 	case "smoke":
-		return test.RunSmoke(dir)
+		smokeFlags := flag.NewFlagSet("nix smoke", flag.ContinueOnError)
+		timeout := smokeFlags.Int("smoke-timeout", 45, "Timeout in seconds for smoke test")
+
+		if len(args) < 2 {
+			return fmt.Errorf("usage: nix smoke <dir> [--smoke-timeout <sec>]")
+		}
+
+		dir := args[1]
+		smokeFlags.Parse(args[2:])
+
+		return test.RunSmoke(dir, *timeout)
+	case "lint":
+		return RunLint()
 	default:
 		return fmt.Errorf("unknown command: %s", command)
 	}
