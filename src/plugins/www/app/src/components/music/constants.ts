97406672f82085028e678d45a2bf2f20736d5f36
Tim <timcash@gmail.com>
2026-02-12 11:00:14 -0800

    feat(www): add harmonic music visualization component

---
+++ src/plugins/www/app/src/components/music/constants.ts
@@ -0,0 +1,5 @@
+export const NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+export const CIRCLE_OF_FIFTHS = ["C", "G", "D", "A", "E", "B", "F#", "Db", "Ab", "Eb", "Bb", "F"];
+export const NOTE_TO_INDEX: Record<string, number> = {
+  "C": 0, "C#": 1, "Db": 1, "D": 2, "D#": 3, "Eb": 3, "E": 4, "F": 5, "F#": 6, "Gb": 6, "G": 7, "G#": 8, "Ab": 8, "A": 9, "A#": 10, "Bb": 10, "B": 11
+};
