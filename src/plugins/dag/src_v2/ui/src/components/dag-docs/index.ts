6c94a449bc9d5c2c4b237c29c5d044465830f20e
Tim <timcash@gmail.com>
2026-02-20 08:55:32 -0800

    cleanup

--- src/plugins/dag/src_v2/ui/src/components/dag-docs/index.ts
+++
@@ -1,8 +0,0 @@
-import type { VisualizationControl } from '../../dialtone-ui'
-
-export async function mountDagDocs(): Promise<VisualizationControl> {
-  return {
-    dispose: () => {},
-    setVisible: (_visible: boolean) => {},
-  }
-}
