456889c9b41059e94c6b2b54b9705b72cf6db058
Tim <timcash@gmail.com>
2026-03-03 11:08:18 -0800

    feat: refactor toolchain to nix-first workflow with automated mesh discovery

--- flake.nix
+++ flake.nix
@@ -1,42 +1,42 @@
 {
-  description = "Dialtone dev shell (nix-first bootstrap)";
-
+  description = "Dialtone dev shell";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-24.11.tar.gz";
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bash
-            curl
-            git
-            gh
-            openssh
-            go
-            gnumake
-            gcc
-            cmake
-            ninja
-            pkg-config
-            nodejs
-          ] ++ (if pkgs.stdenv.isLinux then [
-            pkgs.musl.dev
-            pkgs.glibc.static
-            pkgs.pkgsStatic.gcc
-          ] else [ ]);
-          shellHook = ''
-            export DIALTONE_USE_NIX=1
-            echo "DIALTONE> nix-shell active"
-          '';
-        };
-      }
-    );
+    {
+      devShells = forAllSystems (system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bash curl git gh openssh go_1_24 gnumake gcc cmake ninja pkg-config nodejs tmux zsh cloudflared
+            ] ++ (if pkgs.stdenv.isLinux then [
+              pkgs.musl.dev pkgs.glibc.static pkgs.pkgsStatic.gcc
+            ] else [ ]);
+            shellHook = ''
+              export DIALTONE_REPO_ROOT=$(pwd)
+              export PATH="$DIALTONE_REPO_ROOT/bin:$PATH"
+              
+              # Initialize completions and set up cycling if in ZSH
+              if [ -n "$ZSH_VERSION" ]; then
+                fpath=($ZSH/functions $ZSH/scripts $fpath)
+                autoload -U compinit && compinit -u
+                zstyle ':completion:*' menu select
+                setopt MENU_COMPLETE
+                bindkey '^I' expand-or-complete
+              fi
+
+              export DIALTONE_NIX_ACTIVE=1
+              echo "DIALTONE> nix-shell active"
+            '';
+          };
+        }
+      );
+    };
 }
 
--- src/go.mod
+++ src/go.mod
@@ -1,6 +1,6 @@
 module dialtone/dev
 
-go 1.25.5
+go 1.24.0
 
 require (
 	github.com/bluenviron/gomavlib/v3 v3.3.0
